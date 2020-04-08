6c9d9718f4b833ff1a0d2389eae159658a21ad77
MrGregor228 <romeo.kadaner@hotmail.com>
2020-04-08 19:30:29 +0300

    smooth-scrolling.js

---
+++ js/smooth-scrolling.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $(".navbar").on("click","a", function (event) {
+
+        event.preventDefault();
+ 
+        var id  = $(this).attr('href'),
+ 
+            top = $(id).offset().top;
+         
+        $('body,html').animate({scrollTop: top - 60}, 500);
+    });
+
+    // $(".top_button").on("click","a", function (event) {
+
+    //     event.preventDefault();
+ 
+    //     var id  = $(this).attr('href'),
+ 
+    //         top = $(id).offset().top;
+         
+    //     $('body,html').animate({scrollTop: top}, 500);
+    // });
+    $('.angle-bottom').click(function() {
+        var sectionTo = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(sectionTo).offset().top - 60
+        }, 500);
+    });
+});
