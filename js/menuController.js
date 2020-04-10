329315ffd30c64c993b1107b6c356644761dfdb7
MrGregor228 <romeo.kadaner@hotmail.com>
2020-04-10 11:53:56 +0300

    Updated menu #2 (test of opacity)

--- js/menuController.js
+++ js/menuController.js
@@ -5,14 +5,18 @@ var menuOpen = document.querySelector(".menu-trigger-open"),
 function showMenu() {
 	menuOpen.style.display = "none";
 	menuClose.style.display = "block";
-	menu.style.display = "block";
 	menu.style.opacity = "1";
+	setTimeout(function() {
+		menu.style.display = "block";
+	}, 500);
 }
 function hideMenu() {
 	menuOpen.style.display = "block";
 	menuClose.style.display = "none";
-	menu.style.display = "none";
 	menu.style.opacity = "0";
+	setTimeout(function() {
+		menu.style.display = "none";
+	}, 500);
 }
 console.log(menuOpen);
 console.log(menuClose);
