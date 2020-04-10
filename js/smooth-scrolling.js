498ea4dcad8c80829c71fd4ea885bb01b6cbb9c9
MrGregor228 <romeo.kadaner@hotmail.com>
2020-04-10 13:08:45 +0300

    Updated menu #6 (more smoothy animated showing menu)

--- js/smooth-scrolling.js
+++ js/smooth-scrolling.js
@@ -2,23 +2,25 @@ var menuOpen = document.querySelector(".menu-trigger-open"),
     menuClose = document.querySelector(".menu-trigger-close"),
     menu = document.getElementById('menu');
 
-function showMenu() {
-    menuOpen.style.display = "none";
+function showMenu() {    
     menuOpen.style.opacity = "0";
+    menuClose.style.opacity = "1";
     menuClose.style.display = "block";
-    menu.style.opacity = "1";
+    menu.style.display = "block";
     setTimeout(function() {
-        menu.style.display = "block";
-    }, 500);
+        menu.style.opacity = "1";
+        menuOpen.style.display = "none";
+    }, 300);
 }
 function hideMenu() {
     menuOpen.style.opacity = "1";
     menuOpen.style.display = "block";
-    menuClose.style.display = "none";
+    menuClose.style.opacity = "0";
     menu.style.opacity = "0";
     setTimeout(function() {
         menu.style.display = "none";
-    }, 500);
+        menuClose.style.display = "none";
+    }, 300);
 }
 console.log(menuOpen);
 console.log(menuClose);
