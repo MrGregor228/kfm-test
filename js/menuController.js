96eb8aef36581b4ac10400aebbe94ed35dd4c2d4
MrGregor228 <romeo.kadaner@hotmail.com>
2020-04-10 11:10:04 +0300

    Подобие меню, ещё дорабатываю

--- js/menuController.js
+++ js/menuController.js
@@ -1,13 +1,18 @@
 var menuOpen = document.querySelector(".menu-trigger-open"),
-	menuClose = document.querySelector(".menu-trigger-close");
+	menuClose = document.querySelector(".menu-trigger-close"),
+	menu = document.getElementById('menu');
 
 function showMenu() {
 	menuOpen.style.display = "none";
 	menuClose.style.display = "block";
+	menu.style.display = "block";
+	menu.style.opacity = "1";
 }
 function hideMenu() {
 	menuOpen.style.display = "block";
 	menuClose.style.display = "none";
+	menu.style.display = "none";
+	menu.style.opacity = "0";
 }
 console.log(menuOpen);
 console.log(menuClose);
