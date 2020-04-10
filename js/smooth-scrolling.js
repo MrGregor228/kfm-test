7dd646cda7cbd1ee10af9b647d917d2e4767de85
MrGregor228 <romeo.kadaner@hotmail.com>
2020-04-10 12:02:12 +0300

    Updated menu #3 (smooth navigation to sections)

--- js/smooth-scrolling.js
+++ js/smooth-scrolling.js
@@ -1,8 +1,34 @@
-$(document).ready(function(){
-    $(".navbar").on("click","a", function (event) {
+var menuOpen = document.querySelector(".menu-trigger-open"),
+    menuClose = document.querySelector(".menu-trigger-close"),
+    menu = document.getElementById('menu');
+
+function showMenu() {
+    menuOpen.style.display = "none";
+    menuClose.style.display = "block";
+    menu.style.opacity = "1";
+    setTimeout(function() {
+        menu.style.display = "block";
+    }, 500);
+}
+function hideMenu() {
+    menuOpen.style.display = "block";
+    menuClose.style.display = "none";
+    menu.style.opacity = "0";
+    setTimeout(function() {
+        menu.style.display = "none";
+    }, 500);
+}
+console.log(menuOpen);
+console.log(menuClose);
+
+$(document).ready(function(){    
+
+    $(".menu-link").on("click","a", function (event) {
 
         event.preventDefault();
- 
+
+        hideMenu();
+
         var id  = $(this).attr('href'),
  
             top = $(id).offset().top;
