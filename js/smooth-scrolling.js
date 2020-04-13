4eead82db36999083f4b3cae775895cfd61aa6d1
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-04-13 17:41:54 +0300

    Новая анимация и вид меню

--- js/smooth-scrolling.js
+++ js/smooth-scrolling.js
@@ -3,25 +3,32 @@ var menuOpen = document.querySelector(".menu-trigger-open"),
     menu = document.getElementById('menu');
 
 function showMenu() {    
-    menuOpen.style.opacity = "0";
-    menuClose.style.opacity = "1";
-    menuClose.style.display = "block";
-    menu.style.display = "block";
-    setTimeout(function() {
-        menu.style.opacity = "1";
-        menuOpen.style.display = "none";
-    }, 300);
+    // menuOpen.style.opacity = "0";
+    // menuClose.style.opacity = "1";
+    // menuClose.style.display = "block";
+    // menu.style.display = "block";
+    $("#menu").css("display","block");
+    $("#menu").removeClass("animated 1 slideOutRight delay-.3s");
+    $("#menu").addClass("animated 1 slideInRight delay-.3s");
+    
+    // setTimeout(function() {
+    //     // menu.style.opacity = "1";
+    //     menuOpen.style.display = "none";
+    // }, 300);
 }
 function hideMenu() {
-    menuOpen.style.opacity = "1";
-    menuOpen.style.display = "block";
-    menuClose.style.opacity = "0";
-    menu.style.opacity = "0";
-    setTimeout(function() {
-        menu.style.display = "none";
-        menuClose.style.display = "none";
-    }, 300);
+    // menuOpen.style.opacity = "1";
+    // menuOpen.style.display = "block";
+    // menuClose.style.opacity = "0";
+    // menu.style.opacity = "0";
+    $("#menu").removeClass("animated 1 slideInRight delay-.3s");
+    $("#menu").addClass("animated 1 slideOutRight delay-.3s");
+    // setTimeout(function() {
+    //     // menu.style.display = "none";
+    //     menuClose.style.display = "none";
+    // }, 300);
 }
+//
 console.log(menuOpen);
 console.log(menuClose);
 
--- projects/js/menu.js
+++ projects/js/menu.js
@@ -3,24 +3,11 @@ var menuOpen = document.querySelector(".menu-trigger-open"),
     menu = document.getElementById('menu');
 
 function showMenu() {    
-    menuOpen.style.opacity = "0";
-    menuClose.style.opacity = "1";
-    menuClose.style.display = "block";
-    menu.style.display = "block";
-    setTimeout(function() {
-        menu.style.opacity = "1";
-        menuOpen.style.display = "none";
-    }, 300);
+$("#menu").css("display","block");
+    $("#menu").removeClass("animated 1 slideOutRight delay-.3s");
+    $("#menu").addClass("animated 1 slideInRight delay-.3s");
 }
 function hideMenu() {
-    menuOpen.style.opacity = "1";
-    menuOpen.style.display = "block";
-    menuClose.style.opacity = "0";
-    menu.style.opacity = "0";
-    setTimeout(function() {
-        menu.style.display = "none";
-        menuClose.style.display = "none";
-    }, 300);
+$("#menu").removeClass("animated 1 slideInRight delay-.3s");
+    $("#menu").addClass("animated 1 slideOutRight delay-.3s");
 }
-console.log(menuOpen);
-console.log(menuClose);
