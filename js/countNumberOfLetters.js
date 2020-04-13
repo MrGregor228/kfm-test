08347e0306e1a8363e6fc7174ec8fb4af72de62d
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-04-13 21:22:02 +0300

    Ограничение количества символов на jQuery

---
+++ js/countNumberOfLetters.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  $('.sec2 .card-text').text($('.sec2 .card-text').text().replace(/(\n|\r|\f)/g, ' '));
+  $('.sec2 .card-text').html($(".sec2 .card-text").text().replace(/(.{250}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
+});
