6b494b62d8d539f79a83fc714ab5b4e081c7bbdb
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-04-14 17:48:56 +0300

    Скоро будет норм слайдер, ещё работаю

--- js/owl-start.js
+++ js/owl-start.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
-	var owl = $('.slider-1');
+	var owl = $('.slider-1'),
+		owlSecond = $('.slider-2');
 
 	owl.owlCarousel({// Owl carousel config
 		loop: true,
@@ -34,6 +35,39 @@ $(document).ready(function () {
 			}
 		}
 	});
+
+	owlSecond.owlCarousel({// Owl carousel config
+		loop: true,
+		nav: true,		
+		pagination: true,
+		autoplay: true,
+		autoplayTimeout: 7000,
+		autoplayHoverPause: true,
+		items: 3,
+		// mouseDrag: false,
+		// touchDrag: false,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+				nav: true
+			},
+			768: {
+				items: 2,
+				nav: true
+			},
+			1000: {
+				items: 3,
+				nav: true,
+				loop: true
+			},
+			1280: {
+				items: 3,
+				nav: true,
+				loop: true
+			}
+		}
+	});
 	
 	$('.falseNavigationLeft').click(function() {// Go to the previous item
 		owl.trigger('prev.owl.carousel', [300]);
@@ -41,5 +75,12 @@ $(document).ready(function () {
 	
 	$('.falseNavigationRight').click(function() {// Go to the net item	
 		owl.trigger('next.owl.carousel', [300]);
+	});
+	$('.falseNavigationLeftSecond').click(function() {// Go to the previous item
+		owlSecond.trigger('prev.owl.carousel', [300]);
+	});
+	
+	$('.falseNavigationRightSecond').click(function() {// Go to the net item	
+		owlSecond.trigger('next.owl.carousel', [300]);
 	});
 });
