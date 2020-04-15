b14fd52cba5c435c8023205368b2d17b462e64e9
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-04-15 13:18:46 +0300

    250 words on News slider

--- js/countNumberOfLetters.js
+++ js/countNumberOfLetters.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
   $('.sec2 .card-text').text($('.sec2 .card-text').text().replace(/(\n|\r|\f)/g, ' '));
   $('.sec2 .card-text').html($(".sec2 .card-text").text().replace(/(.{150}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
+
+  $('.sec3 .slider-text').text($('.sec3 .slider-text').text().replace(/(\n|\r|\f)/g, ' '));
+  $('.sec3 .slider-text').html($(".sec3 .slider-text").text().replace(/(.{250}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
 });
