5d77635421308dcfe356c03e4dc862ff3e1e44f5
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-04-15 14:58:40 +0300

    Обновление

--- js/countNumberOfLetters.js
+++ js/countNumberOfLetters.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.sec2 .card-text').text($('.sec2 .card-text').text().replace(/(\n|\r|\f)/g, ' '));
-  $('.sec2 .card-text').html($(".sec2 .card-text").text().replace(/(.{150}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
+  $('.sec2 .card-text').html($(".sec2 .card-text").text().replace(/(.{100}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
 
   $('.sec3 .slider-text').text($('.sec3 .slider-text').text().replace(/(\n|\r|\f)/g, ' '));
   $('.sec3 .slider-text').html($(".sec3 .slider-text").text().replace(/(.{250}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
