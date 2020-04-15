a1ec5f97f6b5fdc4650cd5197c700cf69de582d6
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-04-15 17:04:55 +0300

    мелкие правки

--- js/countNumberOfLetters.js
+++ js/countNumberOfLetters.js
@@ -1,7 +1,26 @@
-$(document).ready(function () {
-  $('.sec2 .card-text').text($('.sec2 .card-text').text().replace(/(\n|\r|\f)/g, ' '));
-  $('.sec2 .card-text').html($(".sec2 .card-text").text().replace(/(.{100}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
+function onLoadAnonsesTexts() {
+  $('.sec2 .one').text($('.sec2 .one').text().replace(/(\n|\r|\f)/g, ' '));
+  $('.sec2 .one').html($(".sec2 .one").text().replace(/(.{100}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
 
-  $('.sec3 .slider-text').text($('.sec3 .slider-text').text().replace(/(\n|\r|\f)/g, ' '));
-  $('.sec3 .slider-text').html($(".sec3 .slider-text").text().replace(/(.{250}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
-});
+  $('.sec2 .two').text($('.sec2 .two').text().replace(/(\n|\r|\f)/g, ' '));
+  $('.sec2 .two').html($(".sec2 .two").text().replace(/(.{100}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
+
+  $('.sec2 .three').text($('.sec2 .three').text().replace(/(\n|\r|\f)/g, ' '));
+  $('.sec2 .three').html($(".sec2 .three").text().replace(/(.{100}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
+}
+onLoadAnonsesTexts();
+
+function onLoadSliderTexts() {
+  $('.slider-text-first').text($('.slider-text-first').text().replace(/(\n|\r|\f)/g, ' '));
+  $('.slider-text-first').html($(".slider-text-first").text().replace(/(.{250}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
+
+  $('.slider-text-second').text($('.slider-text-second').text().replace(/(\n|\r|\f)/g, ' '));
+  $('.slider-text-second').html($(".slider-text-second").text().replace(/(.{250}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
+
+  $('.slider-text-third').text($('.slider-text-third').text().replace(/(\n|\r|\f)/g, ' '));
+  $('.slider-text-third').html($(".slider-text-third").text().replace(/(.{250}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
+
+  $('.slider-text-fourth').text($('.slider-text-fourth').text().replace(/(\n|\r|\f)/g, ' '));
+  $('.slider-text-fourth').html($(".slider-text-fourth").text().replace(/(.{250}).{0,}/, "$1... <i class='fal fa-long-arrow-right'></i>"));
+}
+onLoadSliderTexts();
