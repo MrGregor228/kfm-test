e071335908fa032557bd3b053497bf46040c4697
Alex <guran_sasha@mail.ru>
2020-04-16 22:29:38 +0300

    alex modal projects

--- js/modal.js
+++ js/modal.js
@@ -21,6 +21,10 @@ $('#anonseThree').click(function () {
     $('#modalThird').css("visibility", "visible");
 });
 
+
+//                  News modals
+
+
 $('#newsOne').click(function (e) {
     e.preventDefault();
     $('#modalNewsOne').css("opacity", "1");
@@ -43,4 +47,32 @@ $('#newsFour').click(function (e) {
     e.preventDefault();
     $('#modalNewsFour').css("opacity", "1");
     $('#modalNewsFour').css("visibility", "visible");
+});
+
+
+//                       project modals
+
+
+$('#projectOne').click(function (e) {
+    e.preventDefault();
+    $('#modalProjectOne').css("opacity", "1");
+    $('#modalProjectOne').css("visibility", "visible");
+});
+
+$('#projectTwo').click(function (e) {
+    e.preventDefault();
+    $('#modalProjectTwo').css("opacity", "1");
+    $('#modalProjectTwo').css("visibility", "visible");
+});
+
+$('#projectThree').click(function (e) {
+    e.preventDefault();
+    $('#modalProjectThree').css("opacity", "1");
+    $('#modalProjectThree').css("visibility", "visible");
+});
+
+$('#projectFour').click(function (e) {
+    e.preventDefault();
+    $('#modalProjectFour').css("opacity", "1");
+    $('#modalProjectFour').css("visibility", "visible");
 });
